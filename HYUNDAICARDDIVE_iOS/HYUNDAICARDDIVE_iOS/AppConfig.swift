86369a46e4e0e980a0df06531629728371cd7440
maeng-kim <ehseo0410@gmail.com>
2025-05-14 16:44:30 +0900

    [setting/#14] 인포 파일 설정

---
+++ HYUNDAICARDDIVE_iOS/HYUNDAICARDDIVE_iOS/AppConfig.swift
@@ -0,0 +1,10 @@
+//
+//  AppConfig.swift
+//  HYUNDAICARDDIVE_iOS
+//
+//  Created by MaengKim on 5/14/25.
+//
+
+import Foundation
+
+let BASE_URL = Bundle.main.infoDictionary?["BASE_URL"] as? String ?? ""
