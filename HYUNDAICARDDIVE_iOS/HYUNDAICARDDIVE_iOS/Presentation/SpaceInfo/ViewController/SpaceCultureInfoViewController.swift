dc69bcd51f5aa3252f7cb91004eefc4e5039906c
heoseungjun <gjtmdwns0815@naver.com>
2025-05-16 22:47:04 +0900

    feat: 내비게이션바 언더라인, 자동 백버튼 삭제

--- HYUNDAICARDDIVE_iOS/HYUNDAICARDDIVE_iOS/Presentation/SpaceInfo/ViewController/SpaceCultureInfoViewController.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-final class SpaceCultureDetailViewController: BaseViewController {
-    
-    override func viewWillAppear(_ animated: Bool) {
-        setNavigationBar(type: .spaceAndCulture)
-    }
-}
